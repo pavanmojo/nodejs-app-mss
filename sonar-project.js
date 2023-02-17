b6d5ff77c973d3fcc9cbbfc6fabab13a9ccb591c
Pavan b Rao <122536851+pavanmojo@users.noreply.github.com>
2023-02-18 01:25:15 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,8 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'squ_8f0f22d1da3088b5503c98837f816b0fb1a6ae26 ',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
