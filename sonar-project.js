81fa3309a436f1d3dbefeacdd841fca16c078c1c
Pavan b Rao <122536851+pavanmojo@users.noreply.github.com>
2023-02-18 01:27:18 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.4.235:9000/',
+  serverUrl: 'http://13.233.145.223:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
